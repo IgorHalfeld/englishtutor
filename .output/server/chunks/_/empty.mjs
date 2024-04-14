815c4570097b1cba625648ab27f20f2a2acee9b4
Igor Halfeld <hello@igorluiz.me>
2024-04-14 20:31:32 -0300

    chore: add static files

--- .output/server/chunks/_/empty.mjs
+++
@@ -1,8 +0,0 @@
-const empty = Object.freeze(
-  Object.create(null, {
-    __unenv__: { get: () => true }
-  })
-);
-
-export { empty as default };
-//# sourceMappingURL=empty.mjs.map
--- .output/server/chunks/_/node2.mjs
+++
@@ -1,9 +0,0 @@
-import _satori from 'satori';
-
-const node = {
-  initWasmPromise: Promise.resolve(),
-  satori: _satori
-};
-
-export { node as default };
-//# sourceMappingURL=node2.mjs.map
